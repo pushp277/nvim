5a60964ae56a063de89dd8bd7cd0bdb613c08adb
Pushp Raj <pushp277@gmail.com>
2024-09-29 18:21:40 +0900

    feat(Plugins): Added plugins like gitsigns, todocomments, autopairs

--- lua/tools/lazy.lua
+++ lua/tools/lazy.lua
@@ -11,7 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("tools.plugins", {
+require("lazy").setup({ { import = "tools.plugins", }, {import = "tools.plugins.lsp"} }, {
   checker = {
     enabled = true,
     notify = false,
